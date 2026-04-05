0fac6bbf3dbcda5cd35d83d5c394958c788a3d0a
Mohammed <mohamned313@gmail.com>
2026-04-05 17:56:27 +0300

    Import HTMLResponse for direct HTML serving

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from PIL import Image
-from starlette.responses import PlainTextResponse
+from starlette.responses import HTMLResponse
 
 # basic logging
 logging.basicConfig(level=logging.INFO)
